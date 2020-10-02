af1eb786e440d92b04d4a704c54f6965f71be3f2
Deeptisonth <43122719+Deeptisonth@users.noreply.github.com>
2020-10-02 12:53:29 -0700

    Changing Profile picture

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Deepti Sonth', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/profile.png',
+    profilePhoto: 'img/Profile.png',
     url: 'https://Deeptisonth.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
